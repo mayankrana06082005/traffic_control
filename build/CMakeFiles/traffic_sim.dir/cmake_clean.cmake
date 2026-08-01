d7e0f0d5d3c8a54cdc1b4872545f028d653be552
mayank <mayankrana06082005@gmail.com>
2026-08-01 11:11:28 +0530

    phase1-3

---
+++ build/CMakeFiles/traffic_sim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traffic_sim.dir/phase2.cpp.o"
+  "CMakeFiles/traffic_sim.dir/phase2.cpp.o.d"
+  "traffic_sim"
+  "traffic_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traffic_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
